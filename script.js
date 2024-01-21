6f3fa3bbb5620f90213506c7996bcf7fd00aa371
Marija <marijap@mail.ru>
2024-01-21 14:50:30 +0000

    added user prompt code to JS file

--- script.js
+++ script.js
@@ -29,9 +29,9 @@ var specialCharacters = [
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
+var lowerCasedCharacters = 
+  ['a', 
+  'b', 
   'c',
   'd',
   'e',
@@ -90,15 +90,25 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  
+  var passwordLen = parseInt(prompt("Please enter the length of your password from 8 to 128 characters"));
+    if (passwordLen < 8 || passwordLen > 128) {
+      alert("The password legth is outside of allowed range of characters again");
+    } else {
+        var specialChar = prompt("Please type 'Yes/No' if you would like to include special characters in your password");
+        var numericChar = prompt("Please type 'Yes/No' if you would like to include numeric characters in your password");
+        var upperCasedChar = prompt("Please type 'Yes/No' if you would like to include upper case characters in your password");
+        var lowerCasedChar = prompt("Please type 'Yes/No' if you would like to include lower case characters in your password");
+      }
+    }
 
-
-}
+getPasswordOptions();
 
 // Function for getting a random element from an array
 function getRandom(arr) {
 
 }
+
+console.log(getRandom());
 
 // Function to generate password with user input
 function generatePassword() {
@@ -118,3 +128,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+
