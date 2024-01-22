d7b3e26bc6afebd8f594eea4542a801fdbae847d
Marija <marijap@mail.ru>
2024-01-22 17:17:23 +0000

    added the code to the function which pulls a random element from an array

--- script.js
+++ script.js
@@ -136,6 +136,10 @@ function getPasswordOptions() {
 // Function for getting a random element from an array
 
 function getRandom(arr){
+  
+    var random = arr[Math.floor(Math.random() * arr.length)];
+    console.log(random);
+    return random;
 
 }
 
